f5960ea154d4e0765a741f01fa8bda9f49148706
unknown <morozova.freelance@yandex.ru>
2025-02-05 03:35:34 +0300

    edit ready

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -4,161 +4,187 @@ import { FETCH_ACTIONS } from "/src/actions";
 import axios from "axios";
 
 const Items = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);  
+  let { items, loading, error} = state;    
+  //console.log(items, loading, error);   
 
-    const [state, dispatch] = useReducer(reducer, initialState);  
-    let { items, loading, error} = state;    
-    console.log(items, loading, error);    
+  /*чтение списка из БД*/
+  useEffect(() => {
+    dispatch({type: FETCH_ACTIONS.PROGRESS});    
 
-
-    /*чтение списка из БД*/
-
-    useEffect(() => {
-      dispatch({type: FETCH_ACTIONS.PROGRESS});    
-
-      const getFromDataBase = async () => {
-        try{
-          let response = await axios.get("http://localhost:3000/seminars");
-          if (response.status === 200) {
-            dispatch({type: FETCH_ACTIONS.SUCCESS, data: response.data});
-          }
-        } catch(err){
-          console.error(err);
-          dispatch({type: FETCH_ACTIONS.ERROR, error: err.message})
+    const getFromDataBase = async () => {
+      try{
+        let response = await axios.get("http://localhost:3000/seminars");
+        if (response.status === 200) {
+          dispatch({type: FETCH_ACTIONS.SUCCESS, data: response.data});
         }
+      } catch(err){
+        console.error(err);
+        dispatch({type: FETCH_ACTIONS.ERROR, error: err.message})
       }
-    
-      getFromDataBase();     
-
-    }, []); 
-
-
+    }
   
+    getFromDataBase();  
+  }, []); 
+    
 
-const [isModalOpen, setIsModalOpen] = useState(false)
-const [modalContent, setModalContent] = useState('')
-const [modalType, setModalType] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalContent, setModalContent] = useState('')
+  const [modalType, setModalType] = useState(false)
 
-/* компонент модального окна */
-const Modal = ({ isModalOpen, modalContent, modalType, onClose }) => {
+  /* компонент модального окна */
+  const Modal = ({ isModalOpen, modalContent, modalType, onClose }) => {
 
-  if (isModalOpen !== true) {
-    return null;
-  }  
-  if (modalType == 'delete') { // функция удаления
+    if (isModalOpen !== true) {
+      return null;
+    }  
+    if (modalType == 'delete') { // функция удаления
 
-    const confirmDel = () => {
-      axios.delete('http://localhost:3000/seminars/' + modalContent)
+      const confirmDel = () => {
+        axios.delete('http://localhost:3000/seminars/' + modalContent)
 
-      .then(response => {
-          console.log('Item deleted:', response.data); 
-          items = items.filter(x => {
-            return x.id != modalContent;
-          })
-          console.log('Items refreshed');
-          dispatch({type: FETCH_ACTIONS.SUCCESS, data: items});       
-          setIsModalOpen(false);
-      })
+        .then(response => {
+            console.log('Item deleted:', response.data); 
+            //обновим состояние items
+            items = items.filter(x => {
+              return x.id != modalContent;
+            })
+            console.log('Items refreshed');
+            dispatch({type: FETCH_ACTIONS.SUCCESS, data: items});       
+            setIsModalOpen(false);
+        })
 
-      .catch(error => {
-          console.error('Error deleting item:', error);
-          dispatch({type: FETCH_ACTIONS.ERROR, error: error.message})
-      });   
+        .catch(error => {
+            console.error('Error deleting item:', error);
+            dispatch({type: FETCH_ACTIONS.ERROR, error: error.message})
+        });   
 
-    }
+      }
 
-    return (
-      <section className="modal">
-        <article className="modal-content p-lg-4">
-          <div className="exit-icon text-end">
-            <button onClick={onClose}>Закрыть</button>
-          </div>
-          <main className="modal-mainContents">
-            <h5 className="modal-title">Удалить семинар с id = {modalContent}?</h5>
-            <div className="modal-button text-end">
-              <button onClick={() => confirmDel()}>Да</button> 
+      return (
+        <section className="modal">
+          <article className="modal-content p-lg-4">
+            <div className="exit-icon text-end">
+              <button onClick={onClose}>Закрыть</button>
             </div>
-          </main>
-        </article>
-      </section>
-    );
+            <main className="modal-mainContents">
+              <h5 className="modal-title">Удалить семинар с id = {modalContent}?</h5>
+              <div className="modal-button text-end">
+                <button onClick={() => confirmDel()}>Да</button> 
+              </div>
+            </main>
+          </article>
+        </section>
+      );
 
-  }  else if (modalType == "edit") { // функция редактирования
+    }  else if (modalType == "edit") { // функция редактирования
 
-    const confirmEdit = () => {
-      axios.put('http://localhost:3000/seminars/' + modalContent.id, {
-        id: modalContent.id,
-        title: modalContent.title + "1",
-        description: modalContent.description,
-        date: modalContent.date,
-        time: modalContent.time,
-        photo: modalContent.photo
-      })
-      
-      .then(response => {
-          console.log('Item edited:', response.data);  
+      const confirmEdit = () => {
+              
+        /*сюда переменные из инпутов*/
+        let newTitle = document.querySelector("#newTitle").value;
+        let newDescription = document.querySelector("#newDescription").value;
+        let newDate = document.querySelector("#newDate").value;
+        let newTime = document.querySelector("#newTime").value;
+        let newPhoto = document.querySelector("#newPhoto").value;
 
-          let currebtId = modalContent.id;
+        //запрос
+        axios.put('http://localhost:3000/seminars/' + modalContent.id, {
+          id: modalContent.id,
+          title: newTitle,
+          description: newDescription,
+          date: newDate,
+          time: newTime,
+          photo: newPhoto
+        })
+        
+        .then(response => {
+            console.log('Item edited:', response.data);  
 
-          items = items.map(elem => {
-            if (elem.id === currebtId) {
-              return response.data;
-            } else {
-              return elem;
-            }
-          });
-          
-          
-          console.log('Items refreshed');
-          dispatch({type: FETCH_ACTIONS.SUCCESS, data: items});       
-          setIsModalOpen(false);
-      })
+            let currebtId = modalContent.id;
 
-      .catch(error => {
-          console.error('Error editing item:', error);
-          dispatch({type: FETCH_ACTIONS.ERROR, error: error.message})
-      });   
+            //обновим состояние items
+            items = items.map(elem => {
+              if (elem.id === currebtId) {
+                return response.data;
+              } else {
+                return elem;
+              }
+            });
+            
+            
+            console.log('Items refreshed');
+            dispatch({type: FETCH_ACTIONS.SUCCESS, data: items});       
+            setIsModalOpen(false);
+        })
 
-    }
+        .catch(error => {
+            console.error('Error editing item:', error);
+            dispatch({type: FETCH_ACTIONS.ERROR, error: error.message})
+        });   
 
-    return (
-      <section className="modal">
-        <article className="modal-content p-lg-4">
-          <div className="exit-icon text-end">
-            <button onClick={onClose}>Закрыть</button>
-          </div>
-          <main className="modal-mainContents">
-            <h5 className="modal-title">Сохранить изменения для семинара с id = {modalContent.id}?</h5>
-            <div className="modal-button text-end">
-              <button onClick={() => confirmEdit()}>Да</button> 
+      }
+
+      return (
+        <section className="modal">
+          <article className="modal-content p-lg-4">
+            <div className="exit-icon text-end">
+              <button onClick={onClose}>Закрыть</button>
             </div>
-          </main>
-        </article>
-      </section>
-    );
-  }
-};
+            <main className="modal-mainContents">
+
+              <label>
+                Тема: <input id="newTitle" type="text" defaultValue={modalContent.title}/>
+              </label>
+              <br/>
+              <label>
+              Описание: <input id="newDescription" type="text" defaultValue={modalContent.description}/>
+              </label>
+              <br/>
+              <label>
+              Дата: <input id="newDate" type="text" defaultValue={modalContent.date}/>
+              </label>
+              <br/>
+              <label>
+              Время: <input id="newTime" type="text" defaultValue={modalContent.time}/>
+              </label>
+              <br/>
+              <label>
+              Фото: <input id="newPhoto" type="text" defaultValue={modalContent.photo}/>
+              </label>
+
+              <h5 className="modal-title">Сохранить изменения для семинара с id = {modalContent.id}?</h5>
+              <div className="modal-button text-end">
+                <button onClick={() => confirmEdit()}>Да</button> 
+              </div>
+            </main>
+          </article>
+        </section>
+      );
+    }
+  };
 
 
-/*удаление элемента из БД*/  
-function handleDelete(item) {  
-  console.log('In delete item', item);
-  setIsModalOpen(true);
-  setModalContent(item.id);
-  setModalType('delete');
+  /*удаление элемента из БД*/  
+  function handleDelete(item) {  
+    console.log('In delete item', item);
+    setIsModalOpen(true);
+    setModalContent(item.id);
+    setModalType('delete');
 
-};
+  };
 
-/* редактирование элемента */  
-function handleEdit(item) {  
-  console.log('In edit item', item);
-  setIsModalOpen(true);
-  setModalContent(item);
-  setModalType('edit');
-};
+  /* редактирование элемента */  
+  function handleEdit(item) {  
+    console.log('In edit item', item);
+    setIsModalOpen(true);
+    setModalContent(item);
+    setModalType('edit');
+  };
 
-const closeModal = () => {
-  setIsModalOpen(false);
-};
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
 
   return (
